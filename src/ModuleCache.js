da812ddf9e8f2caa579aa4491b78c04c7bd2b43e
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2024-08-06 22:50:00 +0300

    Format files with prettier

--- src/ModuleCache.js
+++ src/ModuleCache.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
-import fs from 'fs';
-import glob from 'glob';
-import MapCache from './MapCache';
-import Module from 'module';
-import { normalizePath } from './util';
-import path from 'path';
+import _ from "lodash";
+import fs from "fs";
+import glob from "glob";
+import MapCache from "./MapCache";
+import Module from "module";
+import { normalizePath } from "./util";
+import path from "path";
 
 /*----------------------------------------------------------------------------*/
 
@@ -16,45 +16,61 @@ export default class ModuleCache extends MapCache {
     if (!moduleRoot) {
       return;
     }
-    const pkgPath = path.join(moduleRoot, 'package.json');
-    const pkgMain = fs.existsSync(pkgPath) && require(pkgPath).main || 'index.js';
-    const mainPath = normalizePath(path.dirname(path.resolve(moduleRoot, pkgMain)));
+    const pkgPath = path.join(moduleRoot, "package.json");
+    const pkgMain =
+      (fs.existsSync(pkgPath) && require(pkgPath).main) || "index.js";
+    const mainPath = normalizePath(
+      path.dirname(path.resolve(moduleRoot, pkgMain)),
+    );
 
     // Sort paths by the “main” entry first.
-    const dirPaths = _.orderBy(glob.sync(path.join(moduleRoot, '**/'), {
-      'ignore': path.join(moduleRoot, 'node_modules/**/')
-    }), dirPath => _.startsWith(dirPath, mainPath), ['desc']);
+    const dirPaths = _.orderBy(
+      glob.sync(path.join(moduleRoot, "**/"), {
+        ignore: path.join(moduleRoot, "node_modules/**/"),
+      }),
+      (dirPath) => _.startsWith(dirPath, mainPath),
+      ["desc"],
+    );
 
-    _.each(dirPaths, dirPath => {
+    _.each(dirPaths, (dirPath) => {
       const base = path.relative(moduleRoot, dirPath);
-      const filePaths = glob.sync(path.join(dirPath, '*.js'));
-      const pairs = _.map(filePaths, filePath => {
-        const name = path.basename(filePath, '.js');
+      const filePaths = glob.sync(path.join(dirPath, "*.js"));
+      const pairs = _.map(filePaths, (filePath) => {
+        const name = path.basename(filePath, ".js");
         return [name.toLowerCase(), name];
       });
       this.set(base, new MapCache(pairs));
     });
   }
 
-  static resolve(id, from=process.cwd()) {
+  static resolve(id, from = process.cwd()) {
     try {
-      const dirs = path.dirname(Module._resolveFilename(id, _.assign(new Module, {
-        'paths': Module._nodeModulePaths(from)
-      }))).split(path.sep);
+      const dirs = path
+        .dirname(
+          Module._resolveFilename(
+            id,
+            _.assign(new Module(), {
+              paths: Module._nodeModulePaths(from),
+            }),
+          ),
+        )
+        .split(path.sep);
 
       let { length } = dirs;
       while (length--) {
-        const dirSub  = dirs.slice(0, length + 1);
-        const dirPath = dirSub.join('/');
-        const pkgPath = path.join(dirPath, 'package.json');
+        const dirSub = dirs.slice(0, length + 1);
+        const dirPath = dirSub.join("/");
+        const pkgPath = path.join(dirPath, "package.json");
 
-        if ((length && dirs[length - 1] == 'node_modules') ||
-            (fs.existsSync(pkgPath) && require(pkgPath).name == id)) {
+        if (
+          (length && dirs[length - 1] == "node_modules") ||
+          (fs.existsSync(pkgPath) && require(pkgPath).name == id)
+        ) {
           return dirPath;
         }
       }
-      return dirs.join('/');
+      return dirs.join("/");
     } catch (e) {}
-    return '';
+    return "";
   }
-};
+}
--- src/config.js
+++ src/config.js
@@ -1,35 +1,31 @@
-import _ from 'lodash'
-import MapCache from './MapCache'
-import ModuleCache from './ModuleCache'
+import _ from "lodash";
+import MapCache from "./MapCache";
+import ModuleCache from "./ModuleCache";
 
-const defaultIds = [
-  'lodash',
-  'lodash-es',
-  'lodash-compat'
-]
+const defaultIds = ["lodash", "lodash-es", "lodash-compat"];
 
-let oldCwd
-const ids = []
-const modules = new MapCache
+let oldCwd;
+const ids = [];
+const modules = new MapCache();
 
 /*----------------------------------------------------------------------------*/
 
 export default function config({ cwd = process.cwd(), id = defaultIds } = {}) {
   if (oldCwd !== cwd) {
-    oldCwd = cwd
-    modules.clear()
+    oldCwd = cwd;
+    modules.clear();
   }
 
   _.each(_.castArray(id), (id) => {
-    if (! modules.get(id)) {
-      const moduleRoot = ModuleCache.resolve(id, cwd)
+    if (!modules.get(id)) {
+      const moduleRoot = ModuleCache.resolve(id, cwd);
 
       if (moduleRoot) {
-        ids.push(id)
-        modules.set(id, new ModuleCache(moduleRoot))
+        ids.push(id);
+        modules.set(id, new ModuleCache(moduleRoot));
       }
     }
-  })
+  });
 
-  return { ids, modules }
+  return { ids, modules };
 }
