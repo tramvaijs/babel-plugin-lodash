ced0ec1658e6030b82006f5399615ce140f3d934
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2024-08-21 01:14:20 +0300

    Remove old files

--- src/MapCache.ts
+++
@@ -1,7 +0,0 @@
-export class MapCache<K, V> {
-  public readonly map: Map<K, V>;
-
-  constructor(pairs: (readonly [K, V])[] = []) {
-    this.map = new Map(pairs);
-  }
-}
--- src/mapping.ts
+++
@@ -1,3 +0,0 @@
-import { config } from "./config";
-
-export const mapping = config();
