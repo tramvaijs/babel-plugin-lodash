d6b2c19df9a86a7d9b806cd7c49eb78d44e31c67
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2024-08-21 18:55:52 +0300

    Remove tests: async-bound-ramda and react-bootstrap

--- test/mixed-fixtures/react-bootstrap/expected.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _Alert2 = _interopRequireDefault(require("react-bootstrap/lib/Alert"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-/*#__PURE__*/React.createElement(_Alert2.default, null);
